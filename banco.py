452256eff0b02785c9a17d38db27bd854e32a563
Acioly <john.acioly@gmail.com>
2022-05-13 17:57:33 -0300

    fix: Tabela Aluno

--- banco.py
+++ banco.py
@@ -7,4 +7,5 @@ con = lite.connect('dados.db')
 # criando tabelas
 with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE aluno(id INTEGER PRIMARY KEY, nome TEXT, local TEXT, email TEXT, ddd_num INTEGER )")
+    cur.execute("CREATE TABLE aluno(id INTEGER PRIMARY KEY, nome TEXT, matricula INTEGER, email TEXT, telefone "
+                "INTEGER )")
